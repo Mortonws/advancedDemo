7d6c33518c9bed5ad8b4ea83f0f6591febca5255
Mortonws <1017336601@qq.com>
2018-01-08 23:28:29 +0800

    update

--- baselib/src/main/java/com/advanced/baselib/base/BaseActivity.java
+++ baselib/src/main/java/com/advanced/baselib/base/BaseActivity.java
@@ -2,10 +2,8 @@ package com.advanced.baselib.base;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 /**
  * @author by morton_ws on 2017/8/11.
@@ -36,8 +34,4 @@ public abstract class BaseActivity extends AppCompatActivity {
     }
 
     protected abstract int setLayoutId();
-
-    protected <T extends View> T getViewById(@IdRes int resourceId) {
-        return (T) findViewById(resourceId);
-    }
 }
